be18c784787c83a808db45ad516e671f9d5c2f52
Ja'farbek <samatovjafar8@gmail.com>
2023-04-09 18:24:36 +0300

    JafarCode

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use stricks"
+
+const howHol = [];
+
+howHol[0] = prompt("Assalomu alaykum. Ismingizni yozib qoldiring", "Ja'far");
+howHol[1] = prompt("Yaxshimisiz. Iltimos yoshingizni kiriting", "18");
+howHol[0] = prompt("Aha endi. Saytga nima maqsadda kirayotganingizni kiriting (Majburiy emas)", "Ja'far Samatovni saytini kurish maqsadida");
+howHol[0] = prompt("Assalomu alaykum Hujayin parolingizni kiriting", "Siz foydalanuvchi bulsangiz nojuya harakat qilmang.");
