73e87a333859ce8853879f0a9ae21ebcc5e2f0f5
Ja'farbek <samatovjafar8@gmail.com>
2023-04-14 15:14:13 +0300

    qales

--- js/main.js
+++ js/main.js
@@ -6,3 +6,4 @@ howHol[0] = prompt("Assalomu alaykum. Ismingizni yozib qoldiring", "Ja'far");
 howHol[1] = prompt("Yaxshimisiz. Iltimos yoshingizni kiriting", "18");
 howHol[2] = prompt("Aha endi. Saytga nima maqsadda kirayotganingizni kiriting (Majburiy emas)", "Ja'far Samatovni saytini kurish maqsadida");
 howHol[3] = prompt("Assalomu alaykum Hujayin parolingizni kiriting", "Siz foydalanuvchi bulsangiz nojuya harakat qilmang.");
+
